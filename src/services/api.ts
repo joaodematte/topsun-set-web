bb3fc117a700755bf84ae2ccc64df40478955f8d
João Dematte <jgg.dematte@gmail.com>
2022-01-31 08:05:04 -0300

    change api path

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,9 @@
 import Axios from "axios";
 
 const api = Axios.create({
-  baseURL: `${
-    process.env.APP_TYPE == "DEV"
-      ? "http://localhost:3333"
-      : "https://topsun-backend.herokuapp.com"
-  }`,
+  baseURL: "https://topsun-backend.herokuapp.com",
 });
+
+console.log(process.env.APP_TYPE);
 
 export default api;
